e106bd763d4ea6bb12d053e688956616338ae80c
Simon Garnier <garnier@njit.edu>
2018-03-10 12:11:45 -0500

    Should work now

--- R/camera.R
+++ R/camera.R
@@ -9,8 +9,17 @@ initCams <- function() {
     if (is.null(cam)) {
       searching <- FALSE
     } else {
-      cams[[i + 1]] <- cam
-      i <- i + 1
+      pic <- Rvision::readNext(cam)
+      Rvision::display(pic)
+      use <- svDialogs::dlgMessage("Do you want to use this camera?", "yesno")$res
+      Rvision::destroyDisplay()
+      save_dir <- svDialogs::dlgDir(title = "Select folder to save images")$res
+
+      if (use == "yes") {
+        cams[[i + 1]] <- list(cam = cam, save_dir = save_dir)
+        setCam(cams[[i + 1]])
+        i <- i + 1
+      }
     }
   }
 
@@ -18,8 +27,20 @@ initCams <- function() {
 }
 
 #' @export
+setCam <- function(cam) {
+  ini <- svDialogs::dlgOpen(title = "Select camera settings")$res
+  settings <- read.csv(ini, header = FALSE, stringsAsFactors = FALSE)
+
+  for (i in seq_len(nrow(settings))) {
+    Rvision::setProp(cam$cam, settings[i, 1], settings[i, 2])
+  }
+}
+
+#' @export
 releaseCams <- function(cams) {
-  lapply(cams, Rvision::release)
+  for (i in seq_len(length(cams))) {
+    Rvision::release(cams[[i]]$cam)
+  }
 }
 
 #' @export
@@ -27,9 +48,10 @@ grabPictures <- function(cams, n = 30) {
   lapply(cams, function(cam, n) {
     pics <- vector(mode = "list", length = n)
     for (i in seq_len(n)) {
-      pics[[i]] <- Rvision::readNext(cam)
+      pics[[i]] <- Rvision::readNext(cam$cam)
     }
-    Rvision:::mean(pics)
+    file_name <- paste0(cam$save_dir, sprintf("%06d", length(dir(cam$save_dir))), ".png")
+    suppressMessages(Rvision::write.Image(Rvision:::mean(pics), file_name))
   }, n = n)
 }
 
--- R/observR.R
+++ R/observR.R
@@ -1,6 +1,6 @@
 #' @export
 observR <- function(wemo) {
-  initCam(cams)
+  cams <- initCams()
 
   ui <- svDialogs::dlgInput(message = "Duration of the experiment in seconds")
   duration <- as.numeric(gsub("[^0-9]", "", ui$res))
@@ -10,24 +10,23 @@ observR <- function(wemo) {
 
   times <- now() + seq(0, duration, interval) * 1000
 
-  pb <- progress::progress_bar$new(total = 100, format = "Progress [:bar] :percent")
+  pb <- progress::progress_bar$new(total = length(times), format = "Progress [:bar] :percent")
 
-  for (i in 1:length(times)) {
-    lightON(wemo)
+  for (i in seq_len(length(times))) {
+    magicLamp::wemo_ON(wemo)
     Sys.sleep(2)
 
-    grabPicture(cams)
+    grabPictures(cams)
 
-    print(paste0("Last picture taken at: ", Sys.time()))
+    pb$tick()
 
-    lightOFF(wemo)
+    magicLamp::wemo_OFF(wemo)
 
     if (i < length(times)) {
       Sys.sleep((times[i + 1] - now()) / 1000)
     }
-
-    pb$tick()
   }
 
+  releaseCams(cams)
   NULL
 }
