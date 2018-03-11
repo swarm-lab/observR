e73b9d9b6f26341dc17696ca993a2004c23b69a9
Simon Garnier <simon.garnier@gmail.com>
2018-03-11 17:06:36 -0400

    Trying to detect camera id automatically

--- R/camera.R
+++ R/camera.R
@@ -1,27 +1,32 @@
 #' @export
+camsID <- function() {
+  sys <- Sys.info()[1]
+
+  if (sys == "Linux") {
+    as.numeric(gsub("video", "", list.files("/dev/", "video*")))
+  } else {
+    stop("This function was not implemented yet for your OS.")
+  }
+}
+
+#' @export
 initCams <- function() {
+  id <- camsID()
   cams <- list()
-  searching <- TRUE
-  i <- 0
-  while (searching) {
-    cam <- tryCatch(Rvision::stream(i), error = function(e) NULL)
 
-    if (is.null(cam)) {
-      searching <- FALSE
-    } else {
-      for (j in seq_len(30)) {
-        Rvision::display(Rvision::readNext(cam))
-      }
-      use <- svDialogs::dlgMessage("Do you want to use this camera?", "yesno")$res
-      Rvision::destroyDisplay()
-
-      if (use == "yes") {
-        save_dir <- normalizePath(svDialogs::dlgDir(title = "Select folder to save images")$res)
-        cams[[length(cams) + 1]] <- list(cam = cam, save_dir = save_dir)
-        setCam(cams[[length(cams)]])
-      }
+  for (i in seq_len(length(id))) {
+    cam <- Rvision::stream(id[i])
+    for (j in seq_len(30)) {
+      Rvision::display(Rvision::readNext(cam))
     }
-    i <- i + 1
+    use <- svDialogs::dlgMessage("Do you want to use this camera?", "yesno")$res
+    Rvision::destroyDisplay()
+
+    if (use == "yes") {
+      save_dir <- normalizePath(svDialogs::dlgDir(title = "Select folder to save images")$res)
+      cams[[length(cams) + 1]] <- list(cam = cam, save_dir = save_dir)
+      setCam(cams[[length(cams)]])
+    }
   }
 
   cams
